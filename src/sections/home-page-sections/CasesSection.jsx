5165f458bce114905abfba3d43005323f8042231
Shuhrat <kholiqnazarovshuhrat@gmail.com>
2026-08-24 07:07:11 +0500

    create a order-modal-form

--- src/sections/home-page-sections/CasesSection.jsx
+++ src/sections/home-page-sections/CasesSection.jsx
@@ -1,14 +1,17 @@
 import Button from '../../components/UI/Button'
 
-import airplane from '../../assets/home-page-images/ariplane.png'
-import kargo729 from '../../assets/home-page-images/kargo729.png'
-import kargo7292 from '../../assets/home-page-images/kargo729-2.png'
-import ships from '../../assets/home-page-images/ships.png'
+import { useState } from 'react'
 import adress from '../../assets/home-page-images/adress.png'
-import yuan from '../../assets/home-page-images/yuan.png'
+import airplane from '../../assets/home-page-images/ariplane.png'
+import kargo7292 from '../../assets/home-page-images/kargo729-2.png'
+import kargo729 from '../../assets/home-page-images/kargo729.png'
+import ships from '../../assets/home-page-images/ships.png'
 import timer from '../../assets/home-page-images/timer.png'
+import yuan from '../../assets/home-page-images/yuan.png'
+import OrderModalForm from '../../components/UI/Modal'
 
 export function CasesSection() {
+	const [isModalOpen, setIsModalOpen] = useState(false)
 	return (
 		<>
 			<section className='px-2'>
@@ -23,8 +26,8 @@ export function CasesSection() {
 							labore et dolore magna aliqua.
 						</p>
 					</aside>
-					<aside className='bg-[#F4FAFF] rounded-2xl  flex flex-col items-start gap-4 p-4 md:flex-row md:items-center md:justify-between md:w-full md:p-0 md:gap-20	'>
-						<div className='flex flex-col items-start gap-4 md:justify-between md:gap-7 md:items-start'>
+					<aside className='bg-white  rounded-2xl  flex flex-col items-start gap-4 p-4 md:flex-row md:justify-between md:w-full md:p-0 md:gap-20	'>
+						<div className='flex flex-col items-start gap-4 md:justify-between md:gap-5 md:items-start md:p-5'>
 							<Button variant='secondary' className='max-w-37 text-[14px]'>
 								пример кейса
 							</Button>
@@ -67,19 +70,28 @@ export function CasesSection() {
 									</div>
 								</div>
 							</div>
-							<Button className='md:max-w-60'>Заказать</Button>
+							<Button
+								className='md:max-w-60'
+								onClick={() => setIsModalOpen(true)}
+							>
+								Заказать
+							</Button>
 						</div>
 
 						<img src={kargo729} alt='kargo729' className='md:hidden' />
 						<img
 							src={kargo7292}
 							alt='kargo729'
-							className='hidden md:block w-150'
+							className='hidden md:block w-150 h-130'
 						/>
 					</aside>
 					<img src={airplane} alt='airplane' className='w-full md:hidden' />
 					<img src={ships} alt='airplane' className='w-full hidden md:block' />
 				</div>
+				<OrderModalForm
+					isOpen={isModalOpen}
+					onClose={() => setIsModalOpen(false)}
+				/>
 			</section>
 		</>
 	)
